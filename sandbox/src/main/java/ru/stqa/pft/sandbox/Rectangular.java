763fa0b0034d629a38c839affe78bb8713383a62
MargaritaO <solentuna@gmail.com>
2018-11-20 23:27:46 +0300

    Класс Квадрат и Прямоугольник + переработка в программе

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangular.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+
+public class Rectangular {
+
+    public double a;
+    public double b;
+
+    public Rectangular (double a, double b) {
+
+        this.a = a;
+        this.b = b;
+    }
+}
