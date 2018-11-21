4cc7d685fbed75199b45fba7075a0b3060148c98
MargaritaO <solentuna@gmail.com>
2018-11-21 12:44:26 +0300

    Функции для расчета площадей квадрата и прямоугольника преобразованы в методы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,23 +6,15 @@ public class MyFirstProgram {
         hello("user");
         hello("Brusnika");
 
-        Square s = new Square (5);
-        System.out.println ("Площадь квадрата со стороной " + s.l + " = " + area (s));
+        Square s = new Square (5); //Создаем новый объект s типа square (как int) класса Square
+        System.out.println ("Площадь квадрата со стороной " + s.l + " = " + s.area ());
 
-        Rectangular r = new Rectangular(4,6);
-        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+        Rectangular r = new Rectangular(4,6); //Создаем новый объект r типа rectangular (как int) класса Rectangular
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
     }
 
     public static void hello(String somebody) {
         System.out.println("Hello, " + somebody + "!");
     }
-
-    public static double area (Square s) {
-        return s.l * s.l ;
-    }
-
-    public static double area (Rectangular r) {
-        return r.a * r.b;
-    }
-
+    
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangular.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangular.java
@@ -2,12 +2,16 @@ package ru.stqa.pft.sandbox;
 
 public class Rectangular {
 
-    public double a;
+    public double a; // атрибуты а и б типа double для объекта класса Прямоугольник
     public double b;
 
-    public Rectangular (double a, double b) {
+    public Rectangular (double a, double b) { //ф-я Конструктор - инструкция по заполнению параметров объектов этого класса
 
         this.a = a;
         this.b = b;
+    }
+
+    public double area () { //функция, вычисляющая площадь прямоугольника
+        return this.a * this.b;
     }
 }
