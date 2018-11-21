1d84dd890d32a9dca421cb92f6632a0cf43b40f9
MargaritaO <solentuna@gmail.com>
2018-11-21 20:11:25 +0300

    Подправлен класс Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -2,19 +2,11 @@ package ru.stqa.pft.sandbox;
 
 public class Point {
 
-  public double x1;
-  public double y1;
-  public double x2;
-  public double y2;
+  public double x;
+  public double y;
 
-  public Point (double a1, double b1, double a2, double b2) {
-    this.x1 = a1;
-    this.y1 = b1;
-    this.x2 = a2;
-    this.y2 = b2;
-  }
-
-  public double distance() {
-    return Math.sqrt((this.x2-this.x1)*(this.x2-this.x1) + (this.y2-this.y1)*(this.y2-this.y1));
+  public Point (double a, double b) {
+    this.x = a;
+    this.y = b;
   }
 }
