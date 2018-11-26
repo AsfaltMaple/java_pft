e737a6d276937fb85a143cc41b90c55a4687efc6
MargaritaO <solentuna@gmail.com>
2018-11-26 17:12:44 +0300

    Тест на удаление контакта падает (винда, но в том же месте, что и на маке)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DeleteContact.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DeleteContact.java
@@ -6,29 +6,12 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertTrue;
 
 public class DeleteContact extends TestBase {
-  private boolean acceptNextAlert = true;
-//есть строчка с алертом!!
+
 
   @Test
   public void testDeleteContact() throws Exception {
     app.selectContact();
-    acceptNextAlert = true;
     app.deleteSelectedContacts();
-    assertTrue(closeAlertAndGetItsText().matches("^Delete 1 addresses[\\s\\S]$"));
-  }
 
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = app.wd.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
   }
 }
