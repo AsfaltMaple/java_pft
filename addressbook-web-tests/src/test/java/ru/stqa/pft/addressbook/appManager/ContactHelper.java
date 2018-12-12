ed0e0da427a70389df496468967c58c1e5e70233
MargaritaO <solentuna@gmail.com>
2018-12-12 22:37:48 +0300

    Борьба с contactHelper за модификацию контактов через содержание списков

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appManager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appManager/ContactHelper.java
@@ -3,90 +3,90 @@ package ru.stqa.pft.addressbook.appManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class ContactHelper extends BaseHelper {
 
-  public ContactHelper(WebDriver wd) {
-    super(wd);
-  }
-
-  public void returnToHomePage() {
-    click(By.linkText("home"));
-  }
-
-  public void submitContactCreation() {
-    click(By.xpath("(//input[@name='submit'])"));
-  }
-
-
-  public void fillContactForm(ContactData contactData, boolean creation) {
-    type(By.name("firstname"), contactData.getName());
-    type(By.name("lastname"), contactData.getSurname());
-    type(By.name("email"), contactData.getEmail());
-    if (creation) {
-      new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactData.getGroup());
-    } else {
-      Assert.assertFalse(isElementPresent(By.name("new_group")));
+    public ContactHelper(WebDriver wd) {
+        super(wd);
     }
-  }
 
-  public void initNewContact() {
-    click(By.linkText("add new"));
-  }
+    public void returnToHomePage() {
+        click(By.linkText("home"));
+    }
 
-  public void deleteSelectedContacts() {
-    click(By.xpath("//input[@value='Delete']"));
-    wd.switchTo().alert().accept();
-  }
+    public void submitContactCreation() {
+        click(By.xpath("(//input[@name='submit'])"));
+    }
 
-  public void selectContact(int index) {
-    wd.findElements(By.name("selected[]")).get(index).click();
-  }
 
-  public void initContactModification() {
-    click(By.xpath("//img[@alt='Edit']"));
-  }
+    public void fillContactForm(ContactData contactData, boolean creation) {
+        type(By.name("firstname"), contactData.getName());
+        type(By.name("lastname"), contactData.getSurname());
+        type(By.name("email"), contactData.getEmail());
+        if ( creation ) {
+            new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactData.getGroup());
+        } else {
+            Assert.assertFalse(isElementPresent(By.name("new_group")));
+        }
+    }
 
-  public void submitContactModification() {
-    click(By.name("update"));
-  }
+    public void initNewContact() {
+        click(By.linkText("add new"));
+    }
 
-  public void createContact(ContactData contact, boolean b) {
-    initNewContact();
-    fillContactForm(contact, true);
-    submitContactCreation();
-    returnToHomePage();
-  }
+    public void deleteSelectedContacts() {
+        click(By.xpath("//input[@value='Delete']"));
+        wd.switchTo().alert().accept();
+    }
 
-  public boolean isThereAContact() {
-    return isElementPresent(By.name("selected[]"));
-  }
+    public void selectContact(int index) {
+        wd.findElements(By.name("selected[]")).get(index).click();
+    }
 
-  public int getContactCount() {
-    return wd.findElements(By.name("selected[]")).size();
-  }
+    public void initContactModification() {
+        click(By.xpath("//img[@alt='Edit']"));
+    }
 
-  public List<ContactData> getContactList() {
-    List<ContactData> contacts = new ArrayList<ContactData>();
-    List<WebElement> elements = wd.findElements(By.cssSelector("tr[name=entry]")); // строки
-    for (WebElement element : elements) {
-      String id = element.findElement(By.cssSelector("td")).findElement(By.tagName("input")).getAttribute("value");
-      int i;
-      List<WebElement> cells (int i) = element.findElements(By.cssSelector("td")); // получится список ячеек
+    public void submitContactModification() {
+        click(By.name("update"));
+    }
 
-      String lastname = cell.
-      ContactData contact = new ContactData(lastname,name,  null, null);
-      contacts.add(contact);}
+    public void createContact(ContactData contact, boolean b) {
+        initNewContact();
+        fillContactForm(contact, true);
+        submitContactCreation();
+        returnToHomePage();
+    }
 
-      return contacts;
+    public boolean isThereAContact() {
+        return isElementPresent(By.name("selected[]"));
+    }
 
-  }
+    public int getContactCount() {
+        return wd.findElements(By.name("selected[]")).size();
+    }
+
+    public void getCell(int index) {
+        List<WebElement> cell = wd.findElements(By.cssSelector("td[name = entry] > td"));
+    }
+
+    public List<ContactData> getContactList() {
+        List<ContactData> contacts = new ArrayList<ContactData>();
+        List<WebElement> elements = wd.findElements(By.cssSelector("tr[name=entry]")); // строки
+        for (WebElement element : elements) {
+            String id = element.findElement(By.cssSelector("td")).findElement(By.tagName("input")).getAttribute("value");
+            String lastname = getCell(0).getText();
+            String name = getCell(1).getText();
+        }
+        ContactData contact = new ContactData(id, lastname, name, null);
+        contacts.add(contact);
+        return contacts;
+
+    }
 }
