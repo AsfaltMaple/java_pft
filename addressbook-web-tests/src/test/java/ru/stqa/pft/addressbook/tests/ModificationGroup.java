017e74bc96b6cfa6c2c74d1bd374ae0cac4e73c1
MargaritaO <solentuna@gmail.com>
2018-12-14 11:29:57 +0300

    Добавлена сортировка элементов в модификации группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ModificationGroup.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ModificationGroup.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -20,7 +21,7 @@ public class ModificationGroup extends TestBase {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().groupSelection(before.size() - 1);
     app.getGroupHelper().initGroupModification ();
-    GroupData group = new GroupData(before.get(before.size() - 1).getId(),"test1", "test2", "test3");
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(),"test11", "test2", "test3");
     app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification ();
     app.getGroupHelper().returnToGroupPage();
@@ -29,6 +30,9 @@ public class ModificationGroup extends TestBase {
 
     before.remove(before.size() - 1);
     before.add(group);
-    Assert.assertEquals(new HashSet(after), new HashSet(before));
+    Comparator<? super GroupData> byId = (g1 , g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(after, before);
   }
 }
