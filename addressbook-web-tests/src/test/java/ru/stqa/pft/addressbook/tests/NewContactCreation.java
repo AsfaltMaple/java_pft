78e426003e97451d1a799346b7a57f04f03193b0
MargaritaO <solentuna@gmail.com>
2018-12-25 22:51:52 +0300

    Добавлено получение данных для тестов через параметры

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/NewContactCreation.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/NewContactCreation.java
@@ -3,27 +3,37 @@ package ru.stqa.pft.addressbook.tests;
 import org.hamcrest.CoreMatchers;
 import org.hamcrest.MatcherAssert;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
 import javax.sound.midi.Soundbank;
 import java.io.File;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class NewContactCreation extends TestBase {
 
-  @Test
-  public void testNewContact() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validContacts() { //как фотку добавить??
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new ContactData().withSurname("severnaya1").withName("brusnika1").
+            withAddress("NorthPole 1").withGroup("test1")});
+    list.add(new Object[] {new ContactData().withSurname("severnaya2").withName("brusnika2").
+            withAddress("NorthPole 2").withGroup("test1")});
+    list.add(new Object[] {new ContactData().withSurname("severnaya3").withName("brusnika3").
+            withAddress("NorthPole 3").withGroup("test1")});
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validContacts") //как фотку добавить?
+  public void testNewContact(ContactData contact) throws Exception {
     app.goTo().homePage();
     Contacts before = app.contact().all();
     File photo = new File("src/test/resources/574.png");
-    ContactData contact = new ContactData().withSurname("severnaya2").withName("Brusnika2").withEmail("brus2@sever.ru").withGroup("test1").withPhoto(photo);
     app.contact().create(contact, true);
     app.goTo().homePage();
     assertThat(app.contact().getContactCount(), equalTo(before.size() + 1));
