e9b0070ffddaa42806e44f065a92e928a53b1649
MargaritaO <solentuna@gmail.com>
2019-01-21 21:00:49 +0300

    Добавлено отключение капчи через загрузку файла по протоколу ftp

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -13,56 +13,64 @@ import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-  private final Properties properties;
-  private WebDriver wd;
+    private final Properties properties;
+    private WebDriver wd;
 
-  private String browser;
-  private RegistrationHelper registrationHelper;
+    private String browser;
+    private RegistrationHelper registrationHelper;
+    private FtpHelper ftp;
 
 
-  public ApplicationManager(String browser) {
-    this.browser = browser;
-    properties = new Properties();
-  }
-
-  public void init() throws IOException {
-    String target = System.getProperty("target", "local");
-    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-  }
-
-  public void stop() {
-    if (wd != null) {
-      wd.quit();
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+        properties = new Properties();
     }
-  }
 
-  public HttpSession newSession() {
-    return new HttpSession(this);
-  }
-
-  public String getProperty(String key) {
-    return properties.getProperty(key);
-  }
-
-  public RegistrationHelper registration() {
-    if (registrationHelper == null) {
-      registrationHelper =  new RegistrationHelper(this);
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
     }
-    return registrationHelper;
-  }
 
-  public WebDriver getDriver() {
-    if (wd == null) {
-      if ( browser.equals(BrowserType.FIREFOX) ) {
-        wd = new FirefoxDriver();
-      } else if ( browser.equals(BrowserType.CHROME) ) {
-        wd = new ChromeDriver();
-      } else if ( browser.equals(BrowserType.IE) ) {
-        wd = new InternetExplorerDriver();
-      }
-      wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-      wd.get(properties.getProperty("web.baseUrl"));
+    public void stop() {
+        if ( wd != null ) {
+            wd.quit();
+        }
     }
-    return wd;
-  }
+
+    public HttpSession newSession() {
+        return new HttpSession(this);
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+
+    public RegistrationHelper registration() {
+        if ( registrationHelper == null ) {
+            registrationHelper = new RegistrationHelper(this);
+        }
+        return registrationHelper;
+    }
+
+    public FtpHelper ftp() {
+        if ( ftp == null ) {
+            ftp = new FtpHelper(this);
+        }
+        return ftp;
+    }
+
+    public WebDriver getDriver() {
+        if ( wd == null ) {
+            if ( browser.equals(BrowserType.FIREFOX) ) {
+                wd = new FirefoxDriver();
+            } else if ( browser.equals(BrowserType.CHROME) ) {
+                wd = new ChromeDriver();
+            } else if ( browser.equals(BrowserType.IE) ) {
+                wd = new InternetExplorerDriver();
+            }
+            wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+            wd.get(properties.getProperty("web.baseUrl"));
+        }
+        return wd;
+    }
 }
