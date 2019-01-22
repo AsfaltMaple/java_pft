f1cb98da2ed402808e7f78a3dacb2bd28738c35f
MargaritaO <solentuna@gmail.com>
2019-01-22 17:37:24 +0300

    Исправлена загрузка файла через ftp для отключения капчи

--- mantis-tests/out/test/resources/config_inc.php
+++ mantis-tests/out/test/resources/config_inc.php
@@ -1,7 +1,11 @@
 <?php
-$g_host = 'localhost';
-$g_db_type = 'mysql';
-$g_database_name = 'bugtracker';
-$g_db_username = 'root';
-$g_db_password = '';
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+
+$g_default_timezone       = 'UTC';
+
+$g_crypto_master_salt     = 'VxoxqOj/yAIaypjU9b0bc1hKZvQpw182DRzuWAwh77o=';
 $g_signup_use_captcha = OFF;
