180d7409a42307933820a62bc4bb89b26e80ba0f
MargaritaO <solentuna@gmail.com>
2019-01-23 23:02:40 +0300

    Почти сделан тест регистрации пользователя с получением почты на встроенный в тесты почтовый сервер

--- mantis-tests/out/test/resources/config_inc.php
+++ mantis-tests/out/test/resources/config_inc.php
@@ -9,3 +9,6 @@ $g_default_timezone       = 'UTC';
 
 $g_crypto_master_salt     = 'VxoxqOj/yAIaypjU9b0bc1hKZvQpw182DRzuWAwh77o=';
 $g_signup_use_captcha = OFF;
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'localhost';
+$g_default_language = 'english';
