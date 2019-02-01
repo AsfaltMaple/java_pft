1c59d2d009aafccc470ebc70d3b339c1cf24207a
MargaritaO <solentuna@gmail.com>
2019-02-02 01:53:15 +0300

    добавлена интеграция тестов через Rest с Bugify

--- rest-sample/src/test/java/ru/stqa/pft/rest/RestTests.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/RestTests.java
@@ -13,14 +13,19 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 import java.util.Set;
 
+import static com.sun.javafx.runtime.async.BackgroundExecutor.getExecutor;
 import static org.testng.Assert.assertEquals;
 
-public class RestTests {
+public class RestTests extends TestBase{
 
     @Test
     public void testCreateIssue() throws IOException {
+        boolean issueOpen = isIssueOpen(737);
+        if(issueOpen == false) {
+            skipIfNotFixed(737);
+        }
         Set<Issue> oldIssues = getIssues();
-        Issue newIssue = new Issue().withSubject("test issue").withDescription("new test issue");
+        Issue newIssue = new Issue().withSubject("test issue Ritka").withDescription("new test issue");
         int issueId = create(newIssue);
         Set<Issue> newIssues = getIssues();
         oldIssues.add(newIssue.withId(issueId));
@@ -28,7 +33,11 @@ public class RestTests {
     }
 
     private Set<Issue> getIssues() throws IOException {
-        String json = getExecutor().execute(Request.Get("http://bugify.stqa.ru/api/issues.json"))
+        boolean issueOpen = isIssueOpen(736);
+        if(issueOpen == false) {
+            skipIfNotFixed(736);
+        }
+        String json = getExecutor().execute(Request.Get("http://bugify.stqa.ru/api/issues.json?limit=1000"))
                 .returnContent().asString();
         JsonElement parsed = new JsonParser().parse(json);
         JsonElement issues = parsed.getAsJsonObject().get("issues");
@@ -36,7 +45,7 @@ public class RestTests {
     }
 
     private int create(Issue newIssue) throws IOException {
-        String json = getExecutor().execute(Request.Post("http://bugify.stqa.ru/api/issues/mine.json")
+        String json = getExecutor().execute(Request.Post("http://bugify.stqa.ru/api/issues.json")
                 .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject()),
                           new BasicNameValuePair("description", newIssue.getDescription())))
                 .returnContent().asString();
@@ -44,8 +53,7 @@ public class RestTests {
         return parsed.getAsJsonObject().get("issue_id").getAsInt();
 
     }
-
-    private Executor getExecutor() {
-        return Executor.newInstance().auth("288f44776e7bec4bf44fdfeb1e646490", "");
-    }
+   // protected Executor getExecutor() {
+     //   return Executor.newInstance().auth("288f44776e7bec4bf44fdfeb1e646490", "");
+   // }
 }
---
+++ rest-sample/src/test/java/ru/stqa/pft/rest/TestBase.java
@@ -0,0 +1,46 @@
+package ru.stqa.pft.rest;
+
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+import org.testng.SkipException;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Set;
+
+
+public class TestBase {
+
+
+    public boolean isIssueOpen(int issueId) throws IOException {
+        if( Arrays.asList(state_name(issueId)).equals("Resolved")){
+            return true;
+        } else
+        {return false;}
+    }
+      //  return new Gson().fromJson(issues, new TypeToken <Set<Issue>>(){}.getType());
+    public Set<Issue> state_name(int issueId) throws IOException {
+        String json = getExecutor().execute(Request.Get("http://bugify.stqa.ru/api/issues/"  + issueId +".json"))
+                .returnContent().asString();
+        JsonElement parsed = new JsonParser().parse(json);
+        JsonElement issueState = parsed.getAsJsonObject().get("state_name");
+        return new Gson().fromJson(issueState, new TypeToken<Set<Issue>>() {
+        }.getType());
+
+    }
+
+
+    public void skipIfNotFixed(int issueId) throws IOException {
+        if (isIssueOpen(issueId)) {
+            throw new SkipException("Ignored because of issue " + issueId);
+        }
+    }
+
+    protected Executor getExecutor() {
+        return Executor.newInstance().auth("288f44776e7bec4bf44fdfeb1e646490", "");
+    }
+}
